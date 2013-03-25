c201540541b399dad2db27a25b067813ece66773
amalaba <amalaba.thoughtworks.com>
2013-03-26 00:11:53 +0530

    created database converter.db

--- converter_app/models.py
+++ converter_app/models.py
@@ -1,16 +1,31 @@
-from converter_app import converter_db
+from sqlalchemy import *
 
-ROLE_USER=0
-ROLE_ADMIN=1
+def db_create():
+    engine = create_engine('sqlite:///converter.db')
+    engine.echo = True
+    metadata = MetaData(engine)
 
-class Convert_details(converter_db.Model):
+    convert_details = Table('convert_details', metadata,
+                            Column('id', Integer, primary_key=True, autoincrement=True),
+                            Column('convert_attr', VARCHAR(50)),
+                            Column('from_attr', VARCHAR(50)),
+                            Column('to_attr', VARCHAR(50)),
+                            Column('multiplication_factor', Integer)
+    )
 
-    id=converter_db.Column(converter_db.Integer, primary_key=True, autoincrement=True)
-    convert_attr=converter_db.Column(converter_db.String(50)),
-    from_attr=converter_db.Column(converter_db.String(50)),
-    to_attr=converter_db.Column(converter_db.String(50)),
-    multiplication_factor=converter_db.Column(converter_db.Double(50)),
+    metadata.create_all()
 
-def __repr__(self):
-    return 'Conversion on %r' %(self.convert_attr)
+    value = convert_details.insert()
+    value.execute(
+        {'convert_attr': 'Area', 'from_attr': 'square miles', 'to_attr': 'Hectare', 'multiplication_factor': 258.99881},
+        {'convert_attr': 'Currency', 'from_attr': 'USD', 'to_attr': 'INR', 'multiplication_factor': 54.35},
+        {'convert_attr': 'Length', 'from_attr': 'kilometer', 'to_attr': 'meter', 'multiplication_factor': 1000},
+        {'convert_attr': 'Temperature', 'from_attr': 'Celsius', 'to_attr': 'Fahrenheit',
+         'multiplication_factor': 258.99881})
+
+
+if __name__ == "__main__":
+    print "hello"
+    db_create()
+
 
--- db_create.py
+++ db_create.py
@@ -1,5 +1,25 @@
-#!/usr/bin/python
-
-from converter_app import converter_db
-
-converter_db.create_all()
+# #!/usr/bin/python
+#
+# from converter_app import converter_db
+#
+# # converter_db.create_all()
+#
+# convert_details.create()
+#
+# value = convert_details.insert()
+# value.execute(
+#     {'convert_attr': 'Area', 'from_attr': 'square miles', 'to_attr': 'Hectare', 'multiplication_factor': 258.99881},
+#     {'convert_attr': 'Currency', 'from_attr': 'USD', 'to_attr': 'INR', 'multiplication_factor': 54.35},
+#     {'convert_attr': 'Length', 'from_attr': 'kilometer', 'to_attr': 'meter', 'multiplication_factor': 1000},
+#     {'convert_attr': 'Temperature', 'from_attr': 'Celsius', 'to_attr': 'Fahrenheit',
+#      'multiplication_factor': 258.99881})
+#
+# select_val = convert_details.select()
+# rs = select_val.execute()
+#
+# row = rs.fetchone()
+# print('id:', row(0))
+# print('convert_attr', row(1))
+#
+# rs.close()
+#
